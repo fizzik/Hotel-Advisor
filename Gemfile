5f4936634dbb040543ca1b0a3fd116bfad27e993
fizzik <fizzik@ubuntu.ubuntu-domain>
2013-05-07 11:14:00 +0300

    add rating system

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '3.2.12'
-gem 'bcrypt-ruby', '3.0.1'
 
 group :development, :test do
   gem 'mysql2'
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -4,10 +4,24 @@ class Hotel < ActiveRecord::Base
   mount_uploader :image
   has_one :location
   has_many :comment
-
+  has_many :ratings
+  has_many :raters, :through => :ratings, :source => :users
 
 
    accepts_nested_attributes_for :location
 
   validates :title, presence: true, length:  {maximum: 50}
+
+
+  def average_rating
+    @value = 0
+    self.ratings.each do |rating|
+        @value = @value + rating.value
+    end
+    @total = self.ratings.size
+    @value.to_f / @total.to_f
+end
+
+
+
 end
